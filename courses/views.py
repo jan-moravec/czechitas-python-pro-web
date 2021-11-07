53fbd196bf464db2572cec16a3c8b62d9cb0c6b6
Jan Moravec <moravecjan@email.cz>
2021-11-07 11:09:51 +0100

    Add about section

--- courses/views.py
+++ courses/views.py
@@ -4,5 +4,10 @@ from django.views.generic.base import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
 class ContactsView(TemplateView):
     template_name = 'contacts.html'
+
+
+class ContactsView(TemplateView):
+    template_name = 'about.html'
