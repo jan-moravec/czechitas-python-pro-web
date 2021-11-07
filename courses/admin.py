cd99310697b8f30f3c5438ab52f73865f6f597b5
Jan Moravec <moravecjan@email.cz>
2021-11-07 12:23:31 +0100

    Add Branches model

--- courses/admin.py
+++ courses/admin.py
@@ -3,3 +3,5 @@ from . import models
 
 
 admin.site.register(models.Course)
+
+admin.site.register(models.Branch)
--- courses/models.py
+++ courses/models.py
@@ -7,3 +7,13 @@ class Course(models.Model):
     end = models.DateTimeField()
     description = models.CharField(max_length=1000)
     price = models.IntegerField()
+
+
+class Branch(models.Model):
+    city = models.CharField(max_length=100)
+    origin = models.DateField()
+    contact = models.EmailField()
+    employees = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Branches"
