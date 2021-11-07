dd1c969f2290ca65b44715136debff6698929ba6
Jan Moravec <moravecjan@email.cz>
2021-11-07 12:15:42 +0100

    Update database and add model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField()
