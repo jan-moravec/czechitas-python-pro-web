6139411feb28352443e91a41a982dc3523971c27
Jan Moravec <moravecjan@email.cz>
2021-11-07 10:55:30 +0100

    Add contacts

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('contacts', views.ContactsView.as_view(), name='contacts'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class ContactsView(TemplateView):
+    template_name = 'contacts.html'
